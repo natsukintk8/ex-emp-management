079ec9862f6dd48c9653261c52499feff96af8d4
natsukintk8 <oya8n.xwof10@gmail.com>
2026-05-20 15:18:36 +0900

    管理者情報を登録する機能を作成しました
    管理者情報を登録する業務処理を行うメソッドを作成しました
    
    管理者情報を登録する処理を記述しました

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -1,10 +1,12 @@
 package com.example.controller;
 
+import com.example.domain.Administrator;
 import com.example.form.InsertAdministratorForm;
 import com.example.service.AdministratorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -27,5 +29,22 @@ public class AdministratorController {
     @GetMapping("/to-insert")
     public String toInsert(InsertAdministratorForm form){
         return "administrator/insert";
+    }
+
+    /**
+     * 管理者情報を登録する.<br>
+     *
+     * @param form
+     * @return ログイン画面へフォワードする。
+     */
+    @PostMapping("/insert")
+    public String insert(InsertAdministratorForm form){
+        Administrator administrator = new Administrator();
+        administrator.setName(form.getName());
+        administrator.setMailAddress(form.getMailAddress());
+        administrator.setPassword(form.getPassword());
+        administratorService.insert(administrator);
+        return "/";
+
     }
 }

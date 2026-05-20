cfdbcb94b11aa69d0af9fcc0d46e33bc8cbee799
natsukintk8 <oya8n.xwof10@gmail.com>
2026-05-20 11:14:30 +0900

    管理者情報を表すドメインクラスを作成しました
    従業員情報を表すドメインクラスを作成しました
    
    ドメインクラスとリポジトリクラスが完成しました
    
    administratorsテーブルを操作するリポジトリを作成しました
    
    employeesテーブルを操作するリポジトリを作成しました
    
    administratorsテーブルを操作するリポジトリを修正しました

---
+++ src/main/java/com/example/repository/AdministratorRepository.java
@@ -0,0 +1,100 @@
+package com.example.repository;
+
+import com.example.domain.Administrator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class AdministratorRepository {
+    @Autowired
+    private NamedParameterJdbcTemplate template;
+
+    private static final RowMapper<Administrator> ADMINISTRATOR_ROW_MAPPER
+            = (rs, i) -> {
+        Administrator administrator = new Administrator();
+        administrator.setId(rs.getInt("id"));
+        administrator.setName(rs.getString("name"));
+        administrator.setMailAddress(rs.getString("mailAddress"));
+        administrator.setPassword(rs.getString("password"));
+        return administrator;
+    };
+
+    /**
+     * 管理者情報を挿入する.
+     *
+     * @param administrator
+     */
+    public void insert(Administrator administrator) {
+        SqlParameterSource param = new BeanPropertySqlParameterSource(administrator);
+        if (administrator.getId() == null) {
+            String sql = """
+                    INSERT INTO
+                     administrators
+                     (name,mail_address,password)
+                    
+                     VALUES
+                     (:name,:mailAddress,:password)
+                    """;
+
+            template.update(sql, param);
+
+        } else {
+            String sql = """
+                    UPDATE
+                     administrators
+                    
+                     SET
+                     name = :name
+                     ,mail_address = :mailAddress
+                     ,password = :password
+                    
+                     WHERE
+                     id = :id
+                    """;
+
+            template.update(sql, param);
+        }
+    }
+
+    /**
+     * メールアドレスとパスワードから管理者情報を取得する.
+     *
+     * @param mailAddress
+     * @param password
+     * @return　管理者情報（1件も存在しない場合はnullを返す）
+     */
+    public Administrator findByMailAddressAndPassword(String mailAddress, String password) {
+        String sql = """
+                SELECT
+                 id,name,mail_address,password
+                
+                 FROM
+                 administrators
+                
+                 WHERE
+                 mail_address = :mailAddress
+                 AND
+                 password = :password
+                """;
+
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("mail_address", mailAddress)
+                .addValue("password", password);
+
+        List<Administrator> administratorList
+                = template.query(sql, param, ADMINISTRATOR_ROW_MAPPER);
+
+        if (administratorList.isEmpty()) {
+            return null;
+        }
+        return administratorList.getFirst();
+    }
+
+}
---
+++ src/main/java/com/example/repository/EmployeeRepository.java
@@ -0,0 +1,107 @@
+package com.example.repository;
+
+import com.example.domain.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class EmployeeRepository {
+    @Autowired
+    private NamedParameterJdbcTemplate template;
+
+    private static final RowMapper<Employee> EMPLOYEE_ROW_MAPPER
+            = new BeanPropertyRowMapper<>(Employee.class);
+
+    /**
+     * 従業員一覧情報を入社日順（降順）で取得する.
+     *
+     * @return　従業員一覧情報（従業員が存在しない場合はサイズ0件の従業員一覧を返す）。
+     */
+    public List<Employee> findAll() {
+        String sql = """
+                SELECT
+                 id,name,image,gender,hire_date,mail_address,zip_code,address
+                 ,telephone,salary,characteristics,dependents_count
+                
+                 FROM
+                  employees
+                
+                 ORDER BY
+                  hire_code DESC
+                """;
+
+        List<Employee> employeeList = template.query(sql, EMPLOYEE_ROW_MAPPER);
+
+        if (employeeList.isEmpty()) {
+            return null;
+        }
+
+        return employeeList;
+
+    }
+
+    /**
+     * 主キーから従業員情報を取得する.
+     *
+     * @param id
+     * @return　従業員情報（従業員が存在しない場合はSpringが自動的に例外を発生します）。
+     */
+    public Employee findById(Integer id) {
+        String sql = """
+                SELECT
+                 id,name,image,gender,hire_date,mail_address,zip_code,address
+                 ,telephone,salary,characteristics,dependents_count
+                
+                 FROM
+                  employees
+                
+                 WHERE
+                  id = :id
+                """;
+
+        SqlParameterSource param = new MapSqlParameterSource()
+                .addValue("id", id);
+
+        return template.queryForObject(sql, param, EMPLOYEE_ROW_MAPPER);
+    }
+
+    /**
+     * 従業員情報を変更する
+     *
+     * @param employee
+     */
+    public void update(Employee employee) {
+        SqlParameterSource param = new BeanPropertySqlParameterSource(employee);
+
+        String sql = """
+                UPDATE
+                 employees
+                
+                 SET
+                  name = :name
+                 ,image = :image
+                 ,gender = :gender
+                 ,hire_date = :hireDate
+                 ,mail_address = :mailAddress
+                 ,zip_code,address = :zipCode
+                 ,telephone = :telephone
+                 ,salary = :salary
+                 ,characteristics = :characteristics
+                 ,dependents_count = :dependentsCount
+                
+                 WHERE
+                  id = :id
+                """;
+
+        template.update(sql, param);
+
+    }
+}

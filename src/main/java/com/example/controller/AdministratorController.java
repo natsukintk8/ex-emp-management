8973f04b078d997aecac4d9fbeffd4b27fd65ab3
natsukintk8 <oya8n.xwof10@gmail.com>
2026-05-20 14:17:03 +0900

    管理者登録画面を表示する機能を作成しました
    管理者情報を操作するサービスを作成しました
    
    管理者情報登録時に使用するフォームを作成しました
    
    管理者関連機能の処理の制御を行うコントローラを作成しました
    
    管理者登録画面を作成しました

---
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -0,0 +1,31 @@
+package com.example.controller;
+
+import com.example.form.InsertAdministratorForm;
+import com.example.service.AdministratorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 管理者登録画面を表示する処理を記述するコントローラーです.<br>
+ *
+ */
+
+@Controller
+@RequestMapping("/")
+public class AdministratorController {
+    @Autowired
+    private AdministratorService administratorService;
+
+    /**
+     * 従業員登録時のリクエストパラメータを格納します.<br>
+     *
+     * @param form
+     * @return　フォワードするhtmlファイル
+     */
+    @GetMapping("/to-insert")
+    public String toInsert(InsertAdministratorForm form){
+        return "administrator/insert";
+    }
+}
